e2d88bbe05f338d70ba114232df7acac25f41039
Benjamin Barg <benbakerbarg@gmail.com>
2015-02-16 00:23:36 -0500

    progress on learning queue

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Prof. Jason Nieh on wait-free queues and linked lists
 Our first step is to get two threads to increment a shared
 variable. Basically we're working with the GCC CAS primitive so we
 know how to use it in more complicated data structures.
+
+## Next Steps
+
+- add liblfds and concurrency kit queue source to repo for cross referencing
+- team read-through of Kogan and Petrank qait-free queue
+- continue writing our learning version of Michael and Scott queue
--- learn/learnqueue.c
+++ learn/learnqueue.c
@@ -2,8 +2,13 @@
 #include <pthread.h>
 #include <assert.h>
 
+#define __CAS __sync_bool_compare_and_swap
+
 struct node;
 
+/* TODO
+   - propose that pointer_t is 64 bits wide (half pointer, half count)
+ */
 struct pointer_t {
     struct node *ptr;
     unsigned int count;
@@ -36,8 +41,54 @@ new_node(struct node *node, int val)
 int
 init_queue(struct queue *queue, int init_val)
 {
-    queue = (queue *)malloc(sizeof(struct 
+    struct node *init_node;
+    
+    if (new_node(init_node, init_val)) {
+	return -1;
+    }
 
+    queue = (struct queue *) malloc(sizeof(struct queue));
+    if (!queue) {
+	return -1;
+    }
+    
+    queue->head.ptr = init_node;
+    queue->head.count = 0;
+    queue->tail.ptr = init_node;
+    queue->tail.count = 0;
+
+    return 0;
+}
+
+int
+enqueue(struct queue *queue, int val)
+{
+    struct node *node;
+    struct pointer_t tail, next;
+
+    if (new_node(node, val)) {
+	return -1;
+    }
+
+    while (1) {
+	tail = queue->tail;
+	next = tail.ptr->next;
+	if (tail = queue->tail) {
+	    if (next.ptr == NULL) {
+		if (__CAS(&tail.ptr->next,
+			  next,
+			  (struct pointer_t *)
+			  { node, next.count + 1 })) {
+		    break;
+		}
+	    } else {
+		__CAS(&queue->tail,
+		      tail,
+		      (struct pointer_t *)
+		      {
+	    }
+	}
+    }
 
 
 }
