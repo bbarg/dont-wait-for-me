fe41370fa7d5e48d720b255e97f37d07bafe37bc
Benjamin Barg <benbakerbarg@gmail.com>
2015-03-27 00:34:34 -0500

    updated testing framework description

---
+++ bench/README.md
@@ -0,0 +1,20 @@
+# `bench` testing
+
+## Procedure
+
+For each benchmark, create a new folder with a descriptive name. In
+the folder, include two files:
+
+- `procedure.md` that describes your methodology and format of your results
+  + test platform
+  + description of test
+  + what is the test trying to do?
+  + what are hypothesized results?
+  + short summary of what actually happened, including a nice table if
+    possible
+  + link to data dump file
+- data dump file (name whatever you want)
+
+## Notes
+
+Each test should be repeated 5-10 times.
--- bench/arch-x86_84-prelim/test.sh
+++ bench/arch-x86_84-prelim/test.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-echo "----- Running test for arch-x86_64-prelim -----"
-
 if [[ -z $1 ]]; then
     echo "usage: ./test.sh <output file>"
     exit 1
 fi
 
-OUTFILE=$1
+OUTFILE=`pwd`/$1
+touch $OUTFILE
 function decho () {
     echo $1
     echo $1 >> $OUTFILE
 }
 
+function die () {
+    echo -n "Are you sure you want to exit the test [Y/n]? "
+    read res
+    if [[ res -ne "n" ]]; then
+	exit 1
+    fi
+}
+
 set -e
+trap die SIGINT
+
+echo "----- Running test for arch-x86_64-prelim -----"
 
 cd ../../lcrq-server/
 make clean
@@ -23,28 +33,29 @@ make
 servers='http-server lcrq-server msq'
 rates='50 100 125 150 175 200'
 
-for server in servers; do
+for server in $servers; do
 
     decho "----- testing $server ----"
 
     # start server
-    ./$server 8888 ~/html &> /dev/null &
+    random_port=$(( ($RANDOM % 8000) + 2000 ))
+    ./$server $random_port ~/html &
 
     # warmup
     decho "----- warmup for $server -----"
     httperf --hog \
 	    --server localhost \
-	    --port 8888 \
+	    --port $random_port \
 	    --uri /10.html \
 	    --rate 100 \
 	    --num-conn 1000 \
 	    --num-call 1 &> $OUTFILE
     
-    for rate in rates; do
+    for rate in $rates; do
 	decho "----- rate $rate for $server -----"
 	httperf --hog \
 		--server localhost \
-		--port 8888 \
+		--port $random_port \
 		--uri /10.html \
 		--rate $rate \
 		--num-conn 5000 \
