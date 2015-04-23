47a453ca46f5852b93c8a5a49f8792d1839c2063
Benjamin Barg <benbakerbarg@gmail.com>
2015-04-23 15:06:33 -0400

    added debugging statements to latency-performance-v-nthreads/run-test.sh; fixed issue where aforementioned script tries to bind to same port too quickly

--- bench/perf/latency-performance-v-nthreads/run-test.sh
+++ bench/perf/latency-performance-v-nthreads/run-test.sh
@@ -3,23 +3,26 @@
 source ../perf-functions
 
 function start_latency_workload () {
-    httperf --hog --server localhost --port 8888 --uri /281b --num-conns 10000 \
+    port=$1
+    httperf --hog --server localhost --port $port --uri /281b --num-conns 10000 \
 	     &> httperf-$server-$NTHREADS.log &
-    export $httperf_pid=$!
+    export httperf_pid=$!
 }
 
 # run with full ncores since we know that performance is flat across
 # ncores with latency workload
 
-for nthreads in "1 2 4 8 16 32 64"
+for nthreads in 1 2 4 8 16 32 64
 do
     for server in $servers
     do
 	echo "-- Running test for $server with $nthreads threads"
-	start_server $server $nthreads # sets $server_pid and $NTHREADS
-	start_latency_workload
+	port=$(( ($RANDOM % 10000) + 2000 ))
+
+	start_server $server $nthreads $port # sets $server_pid and $NTHREADS
+	start_latency_workload $port 
 	sleep 1
-	start_report $server	# relies on $server_pid
-	wait $server_pid $httperf_pid
+	start_record $server	# relies on $server_pid
+	wait $httperf_pid
     done
 done
